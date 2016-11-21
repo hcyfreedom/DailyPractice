d63a70b291ec2a2d199ecefff10c39aa5b64c549
hcy <956885844@qq.com>
2016-11-21 16:17:16 +0800

    啦啦啦

---
+++ 11-21/addTo.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Acer on 2016/11/21.
+ */
+// 从数组中选择任意两个数，相加等于一个固定值
+window.onload = function () {
+    var source = document.getElementById('source');
+    var submit = document.getElementById('submit');
+    var target = document.getElementById('target');
+
+    submit.onclick = function () {
+
+        var sourceItem = source.value.split(",").map(function(val) {
+            return parseInt(val);
+        });//把字符串转为数字，然后遍历整个函数
+        var targetItem = parseInt(target.value);
+
+
+        for (var i = 0;i<sourceItem.length-1;i++){
+          var  leftValue = targetItem - sourceItem[i];
+
+            // for (var j = 0;j<sourceItem.length-1;j++){
+            //     if (j!= i){
+            //         if (leftValue == sourceItem[j]){
+            //             console.log("完成");
+            //         }
+            //
+            //     }
+            // }
+            var index = sourceItem.indexOf(leftValue);
+            if (index !== -1&&index!=i){
+                console.log(sourceItem[i] +"+"+ sourceItem[index]+ "=" +targetItem);
+
+                break;
+            }
+        }
+    }
+}
